f0c09614c8d7f4240dee760f8d2b16421d037dbb
bichoto024 <matias.mirandamtz@outlook.com>
2024-10-24 17:18:10 +0200

    Update ejercicio3.py
    algunos ajustes en la descripcion

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,19 +1,30 @@
+###########################################################################
 # Este script resuelve el problema numero 3, el cual 
 # describo a continuacion:
-
+#
 # Si corres una carrera de 10 kilómetros en 42 minutos con 
 # 42 segundos, ¿Cuál es tu ritmo promedio 
 # (tiempo por cada milla en minutos y segundos)? 
 # ¿Cuál es tu rapidez promedio en millas por hora?
+###########################################################################
 
+
+# Declaro mis variables
 kilometros = 10
 minutos = 42
 segundos = 42
-tiempo=(minutos*60)+segundos
 millas = 1.61
 
+# Declaro mis formulas
+tiempo=(minutos*60)+segundos
 km_a_millas=kilometros/millas
 velocidad = km_a_millas/tiempo
+
+#########################
+#
+# Imprimo mis resultados
+#
+#########################
 
 print("El tiempo es: %s segundos" %  tiempo) 
 print("%s Millas son 10 Kilometros" % km_a_millas)
