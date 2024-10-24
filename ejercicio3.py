de6a267b695ab321b113215e399ad33d25d13f7d
bichoto024 <matias.mirandamtz@outlook.com>
2024-10-24 17:11:35 +0200

    Update ejercicio3.py
    le di formato a las variables

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,3 +1,6 @@
+# Este script resuelve el problema numero 3, el cual 
+# describo a continuacion:
+
 # Si corres una carrera de 10 kilómetros en 42 minutos con 
 # 42 segundos, ¿Cuál es tu ritmo promedio 
 # (tiempo por cada milla en minutos y segundos)? 
@@ -12,19 +15,13 @@ millas = 1.61
 km_a_millas=kilometros/millas
 velocidad = km_a_millas/tiempo
 
-print("El tiempo es:")
-print(tiempo)
-
-print ("\n")
-
-print("kilometros a millas:")
-print(km_a_millas)
-print ("\n")
+print("El tiempo es: %s segundos" %  tiempo) 
+print("%s Millas son 10 Kilometros" % km_a_millas)
 
 #la velocidad esta en millas por segundos
+print("La velocidad es: %s millas por segundos" % velocidad)
+print ("\n")
 
-print("La velocidad es:")
-print(velocidad)
 
 
 
