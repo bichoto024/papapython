8e4ac9ce5992cb326529e0b42c40f984ef27f17b
bichoto024 <matias.mirandamtz@outlook.com>
2024-11-03 14:15:49 +0100

    ejercicio_volumen_esfera
    Agrego el script del ejercicio 2.2 pagina 15 sobre el calculo del volumen de una esfera

---
+++ volumen_esfera.py
@@ -0,0 +1,11 @@
+# matias miranda martinez
+# 3. 11. 2024
+# El volumen de una esfera con radio r es 4/3*π*r**3. 
+# ¿Cuál es el volumen de una esfera con radio 5?
+
+
+r = 5
+pi = 3.1416
+volumen_esfera = 4/3*pi*r**3
+
+print("EL volumen de la esfera con r = 5 es de %s" % volumen_esfera)
